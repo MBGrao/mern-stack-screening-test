9ca3c70095b4016255acc04386efffeb00d47e0b
ShaheeraMalik <muhammadbingulzar@gmail.com>
2024-06-03 15:42:16 +0500

    Q#2

--- Q#2.js
+++ Q#2.js
@@ -1,6 +1,5 @@
 function q2(str) {
     str = str.toLowerCase();
-    
     for (let i = 0; i < str.length / 2; i++) {
         if (str[i] !== str[str.length - 1 - i]) {
             return false;
@@ -8,8 +7,7 @@ function q2(str) {
     }
     return true;
 }
-
 console.log(q2("radar"));   
-console.log(q2("solo"));  
+console.log(q2("doctor"));  
 console.log(q2("Anna"));   
 
