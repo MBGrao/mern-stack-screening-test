b1e52328d641c8e9e3db5675fbcdd39b28edc41c
ShaheeraMalik <muhammadbingulzar@gmail.com>
2024-06-03 15:37:51 +0500

    Q#2
    Created a function that determines if a given string is a palindrome. The function should take a string as input, convert all characters to lowercase, and then check if the string reads the same from left to right as it does from right to left. If it does, the function returns true otherwise, it returns false.

---
+++ Q#2.js
@@ -0,0 +1,15 @@
+function q2(str) {
+    str = str.toLowerCase();
+    
+    for (let i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(q2("radar"));   
+console.log(q2("solo"));  
+console.log(q2("Anna"));   
+
